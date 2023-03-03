cec805f6a0900d57b49a44b17610ca4ba9c88067
Uralbek Yergenov <oralbekergenov@gmail.com>
2023-03-03 12:12:08 +0600

    Регистрация моделей в админку

--- src/issue_tracker/admin.py
+++ src/issue_tracker/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Type
+from .models import Status
+from .models import Task
+
 # Register your models here.
+admin.site.register(Type)
+admin.site.register(Status)
+admin.site.register(Task)
