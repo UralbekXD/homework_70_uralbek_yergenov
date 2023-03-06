9494b71c7baa5a57a456567f77d0353d40ab1f31
Uralbek Yergenov <oralbekergenov@gmail.com>
2023-03-06 22:55:58 +0600

    Добавить валидаторы
    Сделать валидацию для минимальной и максимальной длины
    Проверить есть ли задачи с таким же кратким описанием

--- src/issue_tracker/forms.py
+++ src/issue_tracker/forms.py
@@ -1,9 +1,43 @@
 from django import forms
+from django.core.validators import BaseValidator, ValidationError
 
 from .models import Task, Type, Status
 
 
+class MinimumLengthValidator(BaseValidator):
+    def __init__(self, min_value):
+        message = 'Value "%(value)s" has length of %(show_value)d! It should be at least %(limit_value)d symbols long!'
+        super().__init__(limit_value=min_value, message=message)
+
+    def compare(self, value, min_value):
+        return value < min_value
+
+    def clean(self, value):
+        return len(value)
+
+
+class MaximumLengthValidator(BaseValidator):
+    def __init__(self, max_value=20):
+        message = 'Value "%(value)s" has length of %(show_value)d! It should be at least %(limit_value)d symbols long!'
+        super().__init__(limit_value=max_value, message=message)
+
+    def compare(self, value, max_value):
+        return value > max_value
+
+    def clean(self, value):
+        return len(value)
+
+
 class TaskForm(forms.ModelForm):
+    short_description = forms.CharField(
+        validators=(MinimumLengthValidator(min_value=2), MaximumLengthValidator(max_value=128)),
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control mb-3'
+            }
+        )
+    )
+
     class Meta:
         model = Task
         fields = [
@@ -21,11 +55,6 @@ class TaskForm(forms.ModelForm):
         }
 
         widgets = {
-            'short_description': forms.TextInput(
-                attrs={
-                    'class': 'form-control mb-3'
-                }
-            ),
             'full_description': forms.Textarea(
                 attrs={
                     'class': 'form-control mb-3'
@@ -44,3 +73,11 @@ class TaskForm(forms.ModelForm):
                 choices=Type.objects.all()
             )
         }
+
+    def clean_short_description(self):
+        short_description = self.cleaned_data.get('short_description')
+        tasks = Task.objects.filter(short_description=short_description)
+        if len(tasks) >= 1:
+            raise ValidationError('Short description like this, already exist!')
+
+        return short_description
